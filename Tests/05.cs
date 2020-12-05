6011833f9f4dfe858179402c5f2db3295365538c
Alison JAROSZ <alison.jarosz@viacesi.fr>
2020-12-05 16:02:38 +0100

    Adds day 5

---
+++ Tests/05.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Days;
+using Xunit;
+
+namespace Tests
+{
+    public class BinaryBoarding
+    {
+        public static readonly string[] example = new string[]
+        {
+            "FBFBBFFRLR",
+            "BFFFBBFRRR",
+            "FFFBBBFRRR",
+            "BBFFBBFRLL"
+        };
+
+        [Fact]
+        public static void FirstStarExample()
+        {
+            var seats = example.Select(s => Days.BinaryBoarding.Partitioning(s)).ToArray();
+            Assert.Equal((44, 5, 357), seats[0]);
+            Assert.Equal((70, 7, 567), seats[1]);
+            Assert.Equal((14, 7, 119), seats[2]);
+            Assert.Equal((102, 4, 820), seats[3]);
+        }
+
+        [Fact]
+        public static void FirstStarSolution()
+        {
+            var sample = Helpers.ReadFile("Inputs\\05.txt");
+
+            Assert.Equal(938, sample.Select(s => Days.BinaryBoarding.Partitioning(s))
+                                  .Max(x => x.seatID));
+        }
+
+        [Fact]
+        public static void SecondStarSolution()
+        {
+            var sample = Helpers.ReadFile("Inputs\\05.txt");
+            var sortedSeats = sample.Select(s => Days.BinaryBoarding.Partitioning(s).seatID).OrderBy(s => s);
+            var mySeat = Days.BinaryBoarding.GetMissingSeat(sortedSeats.ToArray());
+            Assert.Equal(696, mySeat);
+        }
+    }
+}
---
+++ libs/05.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Days
+{
+    public static class BinaryBoarding
+    {
+        public static (int row, int column, int seatID) Partitioning(string str)
+        {
+            string strRow = str[..7];
+            string strColumn = str[7..];
+            var row = DichotomicSearch(strRow, 'F', 'B', 0, 127);
+            var column = DichotomicSearch(strColumn, 'L', 'R', 0, 7);
+
+            return (row, column, row * 8 + column);
+        }
+
+        private static int DichotomicSearch(string str, char min, char max, int valmin, int valmax)
+        {
+            foreach (char c in str)
+            {
+                int diff = (valmax + 1 - valmin + 1) / 2;
+                if (c == min)
+                    valmax -= diff;
+                if (c == max)
+                    valmin += diff;
+            }
+
+            if (valmin == valmax)
+                return valmin;
+            else
+                throw new Exception("Dichotomy search exited without finishing");
+        }
+        public static int GetMissingSeat(int[] arr) 
+            => arr.TakeWhile((x, i) => (x - arr[0]) + 1 == ++i).LastOrDefault() + 1;
+
+        public static string OutputFirst()
+        {
+            return Helpers.ReadFile("Inputs\\05.txt")
+                            .Select(s => Partitioning(s))
+                            .Max(x => x.seatID)
+                            .ToString();
+        }
+        public static string OutputSecond()
+        {
+            var sample = Helpers.ReadFile("Inputs\\05.txt");
+            var sortedSeats = sample.Select(s => Partitioning(s).seatID).OrderBy(s => s);
+            return GetMissingSeat(sortedSeats.ToArray()).ToString();
+        }
+    }
+}
