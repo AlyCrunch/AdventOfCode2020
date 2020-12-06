4841c73e6c9c8998f8f30a9afef9cdc9ab685c48
Alison JAROSZ <alison.jarosz@viacesi.fr>
2020-12-06 22:19:26 +0100

    Adds day 06

---
+++ Tests/06.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Days;
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class CustomCustoms
+    {
+        public static readonly string[] example = new string[]
+        {
+            "abc",
+            "",
+            "a",
+            "b",
+            "c",
+            "",
+            "ab",
+            "ac",
+            "",
+            "a",
+            "a",
+            "a",
+            "a",
+            "",
+            "b"
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var sum = Days.CustomCustoms.CountAnyoneAnsweredYes(example);
+            Assert.Equal(11, sum);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var sum = Days.CustomCustoms.CountAnyoneAnsweredYes(Helpers.ReadFile("Inputs\\06.txt"));
+            Assert.Equal(6504, sum);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var sum = Days.CustomCustoms.CountEveryoneAnsweredYes(example);
+            Assert.Equal(6, sum);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var sum = Days.CustomCustoms.CountEveryoneAnsweredYes(Helpers.ReadFile("Inputs\\06.txt"));
+            Assert.Equal(3351, sum);
+        }
+    }
+}
---
+++ libs/06.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Days
+{
+    public static class CustomCustoms
+    {
+        public static List<List<HashSet<char>>> FormatGroup(string[] strs)
+        {
+            return strs.Aggregate(new List<List<HashSet<char>>> { new List<HashSet<char>>() },
+                                   (list, value) =>
+                                   {
+                                       if (value == "") list.Add(new List<HashSet<char>>());
+                                       else list.Last().Add(new HashSet<char>(value));
+                                       return list;
+                                   });
+
+        }
+
+        public static int CountAnyoneAnsweredYes(string[] strs)
+        {
+            return FormatGroup(strs).Select((list) =>
+            {
+                return list.Aggregate((acc, answers) =>
+                {
+                    acc.UnionWith(answers);
+                    return acc;
+                });
+            }).Sum(x => x.Count);
+        }
+
+        public static int CountEveryoneAnsweredYes(string[] strs)
+        {
+            return FormatGroup(strs).Select((list) =>
+            {
+                return list.Aggregate((acc, answers) =>
+                {
+                    acc.IntersectWith(answers);
+                    return acc;
+                });
+            }).Sum(x => x.Count);
+        }
+
+        public static string OutputFirst()
+            => CountAnyoneAnsweredYes(Helpers.ReadFile("Inputs\\06.txt")).ToString();
+        public static string OutputSecond()
+            => CountEveryoneAnsweredYes(Helpers.ReadFile("Inputs\\06.txt")).ToString();
+    }
+}
