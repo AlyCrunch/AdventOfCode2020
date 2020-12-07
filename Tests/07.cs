a00ae2b7ad7485406eb37c66fbf73fffd2b94e14
Alison JAROSZ <alison.jarosz@viacesi.fr>
2020-12-07 15:44:56 +0100

    Adds Day 7

---
+++ Tests/07.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+    public class HandyHaversacks
+    {
+        public static readonly string[] example = new string[]
+        {
+            "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+            "bright white bags contain 1 shiny gold bag.",
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+            "faded blue bags contain no other bags.",
+            "dotted black bags contain no other bags."
+        };
+
+        public static readonly string[] example2 = new string[]
+        {
+            "shiny gold bags contain 2 dark red bags.",
+            "dark red bags contain 2 dark orange bags.",
+            "dark orange bags contain 2 dark yellow bags.",
+            "dark yellow bags contain 2 dark green bags.",
+            "dark green bags contain 2 dark blue bags.",
+            "dark blue bags contain 2 dark violet bags.",
+            "dark violet bags contain no other bags."
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var bags = Days.HandyHaversacks.FormatBags(example);
+            var shinybags = Days.HandyHaversacks.FindBagInBags("shiny gold", bags);
+            Assert.Equal(4, shinybags.Count);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var bags = Days.HandyHaversacks.FormatBags(Days.Helpers.ReadFile("Inputs\\07.txt"));
+            var shinybags = Days.HandyHaversacks.FindBagInBags("shiny gold", bags);
+            Assert.Equal(238, shinybags.Count);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var first = Days.HandyHaversacks.FormatBags(example);
+            var nbFirst = Days.HandyHaversacks.CountBagsInBag("shiny gold", first);
+            Assert.Equal(32, nbFirst);
+
+            var bags = Days.HandyHaversacks.FormatBags(example2);
+            var nb = Days.HandyHaversacks.CountBagsInBag("shiny gold", bags);
+            Assert.Equal(126, nb);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var bags = Days.HandyHaversacks.FormatBags(Days.Helpers.ReadFile("Inputs\\07.txt"));
+            var nb = Days.HandyHaversacks.CountBagsInBag("shiny gold", bags);
+            Assert.Equal(82930, nb);
+
+        }
+    }
+}
---
+++ libs/07.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Days
+{
+    public static class HandyHaversacks
+    {
+
+        public static Dictionary<string, Dictionary<string, int>> FormatBags(string[] strs)
+        {
+            var bags = strs.Select(x => CleanBagsLine(x))
+                .ToDictionary(str => str.Split(" contain ")[0],
+                                str =>
+                                {
+                                    var splitted = str.Split(" contain ", StringSplitOptions.RemoveEmptyEntries);
+                                    if (splitted.Length == 1)
+                                        return new Dictionary<string, int>();
+                                    else
+                                        return GetSubbags(splitted[1]);
+                                }
+            );
+            return bags;
+        }
+
+        public static List<string> FindBagInBags(string key, Dictionary<string, Dictionary<string, int>> bags)
+        {
+            var listOfBags = new HashSet<string>();
+            var keys = new List<string>() { key };
+
+            while (keys.Count > 0)
+            {
+                keys = bags.Where(x => x.Value.Any(b => keys.Contains(b.Key)))
+                    .Select(x => x.Key).ToList();
+                if (keys.Count > 0) listOfBags.UnionWith(keys);
+            }
+
+            return listOfBags.ToList();
+        }
+
+        public static int CountBagsInBag(string key, Dictionary<string, Dictionary<string, int>> bags)
+        {
+            if (bags[key].Values.Count == 0)
+                return 0;
+            else
+                return bags[key].Sum(x => x.Value + (x.Value * CountBagsInBag(x.Key, bags)));
+        }
+
+        private static string CleanBagsLine(string str)
+        => str.Replace("no other bags", "")
+            .Replace("bags", "")
+            .Replace("bag", "")
+            .Replace("  ", " ")
+            .Replace(".", "");
+
+        private static Dictionary<string, int> GetSubbags(string str)
+        {
+            Dictionary<string, int> formattedBags = new Dictionary<string, int>();
+            foreach (string bag in str.Split(" , "))
+            {
+                var splitted = bag.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                formattedBags.Add(string.Join(" ", splitted[1..]), int.Parse(splitted[0]));
+            }
+
+            return formattedBags;
+        }
+
+        public static string OutputFirst()
+        {
+            var bags = FormatBags(Helpers.ReadFile("Inputs\\07.txt"));
+            return FindBagInBags("shiny gold", bags).Count.ToString();
+        }
+        public static string OutputSecond()
+        {
+            var bags = FormatBags(Helpers.ReadFile("Inputs\\07.txt"));
+            return CountBagsInBag("shiny gold", bags).ToString();
+        }
+    }
+}
