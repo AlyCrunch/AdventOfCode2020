5ae76cdce713fd3d4c9ce98be23a5052f7a40b3d
Alison JAROSZ <alison.jarosz@viacesi.fr>
2020-12-09 00:58:03 +0100

    Adds day 8

---
+++ Tests/08.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+    public class HandheldHalting
+    {
+        public static readonly string[] example = new string[]
+        {
+            "nop +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "jmp -4",
+            "acc +6"
+        };
+
+        [Fact]
+        public void TestFormat()
+        {
+            var ops = Days.HandheldHalting.FormatString(example);
+            Assert.Equal("nop", ops[0].op);
+            Assert.Equal(0, ops[0].arg);
+            Assert.Equal("acc", ops[5].op);
+            Assert.Equal(-99, ops[5].arg);
+        }
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            int acc = Days.HandheldHalting.GetAccumulator(example);
+            Assert.Equal(5, acc);
+        }
+        [Fact]
+        public void FirstStarSolution()
+        {
+            int acc = Days.HandheldHalting.GetAccumulator(Days.Helpers.ReadFile("inputs\\08.txt"));
+            Assert.Equal(1744, acc);
+        }
+        [Fact]
+        public void SecondStarExample()
+        {
+            int acc = Days.HandheldHalting.GetAccumulatorUncorrupted(example);
+            Assert.Equal(8, acc);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            int acc = Days.HandheldHalting.GetAccumulatorUncorrupted(Days.Helpers.ReadFile("inputs\\08.txt"));
+            Assert.Equal(1174, acc);
+        }
+    }
+}
---
+++ libs/08.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Days
+{
+    public static class HandheldHalting
+    {
+        public static (string op, int arg)[] FormatString(string[] input)
+            => input.Select(str =>
+            {
+                var splitted = str.Split(' ');
+                return (splitted[0], int.Parse(splitted[1]));
+            }).ToArray();
+
+        public static int GetAccumulator(string[] list)
+            => FollowInstructions(FormatString(list)).Item2;
+
+        public static int GetAccumulatorUncorrupted(string[] list)
+        {
+            var inst = FormatString(list);
+            var acc = 0;
+
+            for (int i = 0; i < inst.Length - 1; i++)
+            {
+                if (inst[i].op.Equals("acc")) continue;
+
+                var ret = FollowInstructions(((string op, int arg)[])inst.Clone(), i);
+                if (ret.Item1)
+                {
+                    acc = ret.Item2;
+                    break;
+                }
+            }
+
+            return acc;
+        }
+
+        private static Tuple<bool, int> FollowInstructions((string op, int arg)[] inst, int change = -1)
+        {
+            if (change != -1)
+                inst[change].op = inst[change].op.Equals("jmp") ? "nop" : "jmp";
+
+            HashSet<int> history = new HashSet<int>();
+            int acc = 0, index = 0;
+
+            do
+            {
+                history.Add(index);
+                switch (inst[index].op)
+                {
+                    case "nop":
+                        index++;
+                        break;
+                    case "acc":
+                        acc += inst[index].arg;
+                        index++;
+                        break;
+                    case "jmp":
+                        index += inst[index].arg;
+                        break;
+                }
+            } while (!history.Contains(index) && index < inst.Length);
+
+            return new Tuple<bool, int>(index >= inst.Length, acc);
+        }
+    }
+}
