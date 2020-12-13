42b9b6fad2665a566d948629364e95726476ed52
Alison JAROSZ <alison.jarosz@viacesi.fr>
2020-12-13 17:49:08 +0100

    Adds day 10 (de con)

---
+++ Tests/10.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class AdapterArray
+    {
+        public readonly int[] example = new int[]
+        {
+            16,
+            10,
+            15,
+            5,
+            1,
+            11,
+            7,
+            19,
+            6,
+            12,
+            4
+        };
+        public readonly int[] largerExample = new int[]
+        {
+            28,
+            33,
+            18,
+            42,
+            31,
+            14,
+            46,
+            20,
+            48,
+            47,
+            24,
+            23,
+            49,
+            45,
+            19,
+            38,
+            39,
+            11,
+            1,
+            32,
+            25,
+            35,
+            8,
+            17,
+            7,
+            9,
+            4,
+            2,
+            34,
+            10,
+            3
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            int solution = Days.AdapterArray.GetJoltDifferences(example);
+            Assert.Equal(35, solution);
+
+            int solutionLarger = Days.AdapterArray.GetJoltDifferences(largerExample);
+            Assert.Equal(220, solutionLarger);
+        }
+        [Fact]
+        public void FirstStarSolution()
+        {
+            int solution = Days.AdapterArray.GetJoltDifferences(Days.Helpers.ReadFileAsInteger("Inputs\\10.txt"));
+            Assert.Equal(2775, solution);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var solution = Days.AdapterArray.CountArrangements(example);
+            Assert.Equal(8, solution);
+
+            var solutionLarger = Days.AdapterArray.CountArrangements(largerExample);
+            Assert.Equal(19208, solutionLarger);
+        }
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var input = Days.Helpers.ReadFileAsInteger("Inputs\\10.txt");
+            var solution = Days.AdapterArray.CountArrangements(input);
+            Assert.Equal(518344341716992, solution);
+        }
+    }
+}
---
+++ libs/10.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Days
+{
+    public static class AdapterArray
+    {
+        public static int GetJoltDifferences(int[] jolts)
+        {
+            int one = 0, three = 1;
+            int currJolts = 0, jolt = 0;
+
+            var joltsList = jolts.OrderBy(x => x).ToList();
+
+            do
+            {
+                jolt = joltsList.Where(x => 1 <= (x - currJolts) && (x - currJolts) <= 3).FirstOrDefault();
+                joltsList.Remove(jolt);
+                if (jolt - currJolts == 3) three++;
+                if (jolt - currJolts == 1) one++;
+                currJolts = jolt;
+            }
+            while (jolt != 0);
+
+            return three * one;
+        }
+
+        public static int CountArrangements(List<int> jolts, int index = 0)
+        {
+            if (index == 0)
+                jolts.Insert(0, 0);
+
+            do
+            {
+                var p = jolts.Skip(index + 1).Take(3)
+                    .Where(x => 1 <= (x - jolts[index]) && (x - jolts[index]) <= 3).ToList();
+
+                if (p.Count() == 0 && index == jolts.Count - 1)
+                    break;
+
+                index = jolts.IndexOf(p.First());
+
+                if (p.Count() == 1) continue;
+
+                return p.Select(x => CountArrangements(jolts, jolts.IndexOf(x)))
+                    .Sum();
+            }
+            while (index < jolts.Count);
+
+            return 1;
+        }
+
+        public static long CountArrangements(int[] jolts)
+        {
+            var list = jolts.OrderBy(x => x).ToList();
+            list.Insert(0, 0);
+            list.Add(list.Last() + 3);
+
+            List<List<int>> diffs = new List<List<int>>() { new List<int>() };
+            for (int i = 1; i < list.Count; i++)
+            {
+                var diff = list[i] - list[i - 1];
+                switch (diff)
+                {
+                    case 1:
+                        diffs.Last().Add(diff);
+                        break;
+                    case 2:
+                        diffs.Last().Add(diff);
+                        break;
+                    case 3:
+                        diffs.Add(new List<int>());
+                        break;
+                }
+            }
+
+            return diffs.Aggregate((long)1, (acc, values) => acc * Simplify(values));
+        }
+
+        private static long Simplify(List<int> part)
+        {
+            if (part.Count <= 1) return 1;
+            if (part[0] + part[1] > 3) return 1;
+
+            var sum = part[0] + part[1];
+
+            var left = part.Skip(1).ToList();
+            var right = part.Skip(1).ToList();
+            right[0] = sum;
+
+            return new List<List<int>> { left, right }.Sum(x => Simplify(x));
+        }
+
+        public static string OutputFirst()
+            => GetJoltDifferences(Helpers.ReadFileAsInteger("Inputs\\10.txt")).ToString();
+        public static string OutputSecond()
+            => CountArrangements(Helpers.ReadFileAsInteger("Inputs\\10.txt")).ToString();
+    }
+}
