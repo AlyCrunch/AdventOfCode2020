42302c15cd7008b85ac6ebe814d93f660214d8bd
Alison JAROSZ <alison.jarosz@viacesi.fr>
2020-12-15 18:14:03 +0100

    Adds day 15

---
+++ Tests/15.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests
+{
+    public class RambunctiousRecitation
+    {
+        public static readonly string example = "0,3,6";
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var val = Days.RambunctiousRecitation.GetMemoryOptimized(example, 2020);
+            Assert.Equal(436, val);
+        }
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var val = Days.RambunctiousRecitation.GetMemoryOptimized("1,2,16,19,18,0", 2020);
+            Assert.Equal(536, val);
+        }
+        [Fact]
+        public void SecondStarExample()
+        {
+            var val = Days.RambunctiousRecitation.GetMemoryOptimized(example, 30000000);
+            Assert.Equal(175594, val);
+        }
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var val = Days.RambunctiousRecitation.GetMemoryOptimized("1,2,16,19,18,0", 30000000);
+            Assert.Equal(24065124, val);
+        }
+    }
+}
---
+++ libs/15.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Days
+{
+    public static class RambunctiousRecitation
+    {
+        public static int GetMemoryGameResultAt(string inputstr, int turn)
+        {
+            Dictionary<int, int> mem = new Dictionary<int, int>();
+            int index = 0;
+            foreach (string str in inputstr.Split(","))
+            {
+                mem.Add(index, int.Parse(str));
+                index++;
+            }
+
+            for (int i = mem.Count; i < turn; i++)
+            {
+                var lastSpoken = mem[i - 1];
+                var lastMatches = mem.Take(i)
+                    .Where(t => t.Value == lastSpoken)
+                    .Reverse()
+                    .Take(2);
+                if (lastMatches.Count() < 2)
+                    mem[i] = 0;
+                else
+                {
+                    mem[i] = (i - 1) - lastMatches.Last().Key;
+                }
+            }
+
+            return mem[turn - 1];
+        }
+
+        public static int GetMemoryOptimized(string inputstr, int turn)
+        {
+            var input = inputstr.Split(",")
+                                .Select(int.Parse)
+                                .ToArray();
+            var mem = new int[turn];
+            Array.Fill(mem, -1);
+
+            for (int i = 1; i < input.Length + 1; i++)
+                mem[input[i - 1]] = i;
+
+            var value = 0;
+            for (int i = input.Length + 1; i < turn; i++)
+            {
+                var lastSpoken = mem[value];
+                mem[value] = i;
+                value = lastSpoken != -1 ? i - lastSpoken : 0;
+            }
+            return value;
+        }
+
+        #region Printing methods
+        public static string OutputFirst()
+            => GetMemoryOptimized("1,2,16,19,18,0", 2020).ToString();
+        public static string OutputSecond()
+            => GetMemoryOptimized("1,2,16,19,18,0", 30000000).ToString();
+        #endregion
+    }
+}
